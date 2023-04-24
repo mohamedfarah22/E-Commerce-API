070d9c4f91b4d85987a25b1fbb54e03f958d7407
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-04-24 18:49:48 +1000

    created tests and code for checkbox gthat disabled and enables button

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [buttonColor, setButtonColor] = useState('red')
+  const [disabled, setDisabled] = useState(false)
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button style={{backgroundColor: buttonColor}}
+      onClick = {() => setButtonColor(newButtonColor)}
+      disabled={disabled}
+      
+      >Change to {newButtonColor}</button>
+      <input type='checkbox' defaultChecked={disabled} onChange = {(e) => setDisabled(e.target.checked)}></input>
     </div>
+     
+
   );
 }
 
--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,8 +1,47 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { logRoles } from '@testing-library/react';
+test('button has correct inital color, and updates when clicked', () => {
+ render(<App/>);
+//find an element with a role button and text of 'change to blue'
+  const button = screen.getByRole('button', {name:'Change to blue'})
+  //expect background color to be red
+  expect(button).toHaveStyle('background-color:red')
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
+  //click button
+fireEvent.click(button);
+//expect to have correct background color after click
+expect(button).toHaveStyle('background-color:blue')
+
+//expect text to be "Change to red"
+expect(button).toHaveTextContent('Change to red')
 });
+
+test('initial conditions', () =>{
+  render(<App/>)
+  //check that the button starts out enable
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+  expect(colorButton).toBeEnabled();
+
+  //check that the checkbox starts out unchecked
+  const checkBox = screen.getByRole('checkbox')
+  expect(checkBox).not.toBeChecked()
+
+  
+})
+
+test('button disabled functionality', () =>{
+  render(<App/>)
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+  const checkBox = screen.getByRole('checkbox')
+  //check that checkbox is checked when clicked and that button is disabled
+  fireEvent.click(checkBox);
+  expect(checkBox).toBeChecked()
+  expect(colorButton).toBeDisabled()
+
+  //check that checkbox is disabled when clicked again and that button is enabled
+  fireEvent.click(checkBox);
+  expect(checkBox).not.toBeChecked()
+  expect(colorButton).toBeEnabled()
+
+})
