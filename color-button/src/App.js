218d3ae1253c7396ca21be0f0ebf4ef0eb50f3bf
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-04-25 16:09:53 +1000

    Test and code function to transform camel case colors to spaced colors

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+export function replaceCamelCaseWithSpaces(colorName){
+  return colorName.replace(/\B([A-Z])\B/g, ' $1')
+
+}
 function App() {
   const [buttonColor, setButtonColor] = useState('red')
   const [disabled, setDisabled] = useState(false)
@@ -9,10 +13,11 @@ function App() {
 
 
 
+
   
   return (
     <div>
-      <button style={{backgroundColor: buttonColor}}
+      <button style={{backgroundColor: disabled ? 'gray':buttonColor}}
       onClick = {() => setButtonColor(newButtonColor)}
       disabled={disabled}
       
--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 import { logRoles } from '@testing-library/react';
+import { replaceCamelCaseWithSpaces } from './App';
 test('button has correct inital color, and updates when clicked', () => {
  render(<App/>);
 //find an element with a role button and text of 'change to blue'
@@ -38,10 +39,59 @@ test('button disabled functionality', () =>{
   fireEvent.click(checkBox);
   expect(checkBox).toBeChecked()
   expect(colorButton).toBeDisabled()
+ 
 
   //check that checkbox is disabled when clicked again and that button is enabled
   fireEvent.click(checkBox);
   expect(checkBox).not.toBeChecked()
   expect(colorButton).toBeEnabled()
+  
 
+})
+
+test('disabled button has grey background reverts to red', () =>{
+  render(<App/>)
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+  const checkBox = screen.getByRole('checkbox', {name: 'Disable button'})
+  //click checkbox to disable button, button is gray
+  fireEvent.click(checkBox)
+  expect(colorButton).toHaveStyle('background-color: gray')
+  //click checkbox to disable button, button is red
+  fireEvent.click(checkBox)
+  expect(colorButton).toHaveStyle('backgroundColor: red')
+
+
+})
+
+test('clicked button disabled has gray background then reverts to blue', () =>{
+  render(<App/>)
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+  const checkBox = screen.getByRole('checkbox', {name: 'Disable button'})
+
+  //first click button then make sure button background color is blue
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle('background-color: blue');
+
+  //then click checkbox and make sure that button background is gray
+  fireEvent.click(checkBox)
+  expect(colorButton).toHaveStyle('background-color: gray');
+
+  //then click checkbox again and color should revert back to blue
+  fireEvent.click(checkBox);
+  expect(colorButton).toHaveStyle('background-color: blue')
+})
+
+describe('spaces before camel-case capital letters', () => {
+  test('Works for no inner capital letters', () => {
+    expect(replaceCamelCaseWithSpaces('Red')).toBe('Red');
+  })
+  test('Works for one inner capital letter', () =>{
+    expect(replaceCamelCaseWithSpaces('MidnightBlue')).toBe('Midnight Blue')
+
+
+  })
+  test('Works for multiple inner capital letters', () => {
+    expect(replaceCamelCaseWithSpaces('MediumVioletRed')).toBe('Medium Violet Red')
+
+  })
 })
