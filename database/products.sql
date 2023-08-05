5b4cab30733f8df608cefb0c8f7aca14b5e444f9
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-08-05 22:43:15 +1000

    feat: added script to create products table in db ecommercedatabase to run on docker container

--- database/products.sql
+++ database/products.sql
@@ -1,3 +1,8 @@
+--Create a new database
+CREATE DATABASE ecommercedatabase;
+-- Create a new user and grant priveliges
+CREATE USER admin WITH ENCRYPTED PASSWORD 'ecommdb';
+GRANT ALL PRIVILEGES ON DATABASE ecommercedatabase TO admin;
 -- CREATE TABLE 
 DROP TABLE IF EXISTS products;
 CREATE TABLE products (
