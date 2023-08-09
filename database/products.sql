51f44440428cbcfe53790da3ebbb1cd49306645d
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-08-09 17:00:55 +1000

    fix: inserted dummy data in products table to test products on endpoint on postman

--- database/products.sql
+++ database/products.sql
@@ -10,3 +10,12 @@ CREATE TABLE products (
     available_quantity INTEGER,
     image_url VARCHAR
 );
+
+INSERT INTO products (product_description, category, price, available_quantity, image_url)
+VALUES ('21 Carat necklace weighing 12 grams', 'necklaces', 4800.9, 10, 'www.necklace-images.com');
+
+INSERT INTO products (product_description, category, price, available_quantity, image_url)
+VALUES ('18 Carat necklace weighing 7 grams', 'necklaces', 3800.9, 8, 'www.necklace-images.com');
+
+INSERT INTO products (product_description, category, price, available_quantity, image_url)
+VALUES ('10 Carat necklace weighing 9 grams', 'necklaces', 1800.9, 9, 'www.necklace-images.com');
