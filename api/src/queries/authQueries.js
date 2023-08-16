55ea01170f4b0a2c0c3541532a2b9731f7b0c608
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-08-16 21:59:50 +1000

    create new local strategy for authentication by using bcrypt and comparing stored pw with pw sent with login post

--- api/src/queries/authQueries.js
+++ api/src/queries/authQueries.js
@@ -1,5 +1,6 @@
 const Pool = require('pg').Pool;
 const bcrypt = require('bcryptjs')
+const LocalStrategy = require('passport-local');
 const pool = new Pool({
     user:"admin",
     password: "ecommdb",
@@ -38,6 +39,47 @@ const createUser = async (req, res) => {
     
 }
 
+//password comparison logic using bcrypt.js
+const comparePasswords = async (password, hash) => {
+    try {
+        const matchFound = await bcrypt.compare(password, hash)
+        return matchFound
+    } catch (err) {
+        console.log(err)
+    }
+
+    return false
+};
+//create local passport strategy
+
+const strategy = new LocalStrategy(function( email, password, done){
+    pool.query('SELECT * FROM users WHERE email = $1', [email], async function(err, results){
+        if(err){
+            return done(err)
+        }
+
+        if(results.rows.length === 0){
+            return done(null, false, {message: 'Incorrect email or password'})
+        }
+
+        const user = results.rows[0]
+
+        const passwordAuthenticated = await comparePasswords(password, user.password)
+
+        if(passwordAuthenticated){
+            return done(null, user)
+        }
+        else{
+            return done(null, false, {message: 'Incoreect password. '})
+        }
+
+    });
+
+})
+
+
+
 module.exports = {
-    createUser
+    createUser, 
+    strategy
 }
