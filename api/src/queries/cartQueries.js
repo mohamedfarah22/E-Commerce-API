3dd0d11ed2ac0bd87e21e03c5e2d4b9f38797f13
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-08-22 21:30:52 +1000

    add getUserCart and createCartItem helper functions

--- api/src/queries/cartQueries.js
+++ api/src/queries/cartQueries.js
@@ -16,7 +16,34 @@ const getAllCarts = (req, res) => {
         res.status(200).json(results.rows)
     })
 }
+//createCart
 
+//create a cart item
+const createCartItem = (req, res) => {
+    const {user_id, product_id, quantity} = req.body
+    
+    pool.query('INSERT INTO carts(user_id, product_id, quantity ) VALUES ($1, $2, $3) RETURNING *', [user_id, product_id, quantity], (error, results) => {
+        if(error){
+            res.status(400).send(error)
+        }
+        res.status(201).send(`Cart added with ${results.rows[0].id}`)
+
+    } )
+
+}
+
+const getUserCart = (req, res) => {
+    const user_id = parseInt(req.params.user_id)
+
+    pool.query('SELECT * FROM carts WHERE user_id = $1', [user_id], (error, results) => {
+        if(error){
+            res.status(400).send(error)
+        }
+        res.status(200).send(results.rows)
+    })
+}
 module.exports ={
-    getAllCarts
+    getAllCarts, 
+    createCartItem,
+    getUserCart
 }
