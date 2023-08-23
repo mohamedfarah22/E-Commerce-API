d65b926c0e10d2cd9fda0a1587302b8c777d3f7d
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-08-23 17:02:07 +1000

    changed server port from 3000 to 4000 to allow for frontend to be hosted on 3000

--- api/Server.js
+++ api/Server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 4000
 const passport = require('passport')
 const session = require('express-session')
 //require products router
